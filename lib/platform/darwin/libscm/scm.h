78236939c5ca614640d65cd546195dbe8cc82f77
evilbinary <rootntsd@gmail.com>
2017-01-06 02:50:03 +0800

    更新库

--- lib/platform/darwin/libscm/scm.h
+++ lib/platform/darwin/libscm/scm.h
@@ -11,47 +11,61 @@
     typedef void *ptr;
     typedef int iptr;
     typedef unsigned int uptr;
+
+    #define scm_fixnum(x) ((ptr)(uptr)((x)*4))
+    #define scm_char(x) ((ptr)(uptr)((x)<<8|0x16))
+    #define scm_nil ((ptr)0x26)
+    #define scm_true ((ptr)0xE)
+    #define scm_false ((ptr)0x6)
+    #define scm_boolean(x) ((x)?Strue:Sfalse)
+    #define scm_bwp_object ((ptr)0x4E)
+    #define scm_eof_object ((ptr)0x36)
+    #define scm_void ((ptr)0x3E)
 #else
     #ifdef __x86_64__
 
     typedef void * ptr;
     typedef long int iptr;
     typedef unsigned long int uptr;
-       
+    
+    #define scm_fixnum(x) ((ptr)(uptr)((x)*8))
+    #define scm_char(x) ((ptr)(uptr)((x)<<8|0x16))
+    #define scm_nil ((ptr)0x26)
+    #define scm_true ((ptr)0xE)
+    #define scm_false ((ptr)0x6)
+    #define scm_boolean(x) ((x)?scm_true:scm_false)
+    #define scm_bwp_object ((ptr)0x4E)
+    #define scm_void ((ptr)0x3E)
+    #define scm_eof_object ((ptr)0x36)
+
     #else
     typedef void * ptr;
     typedef int iptr;
     typedef unsigned int uptr;
+
+    #define scm_fixnum(x) ((ptr)(uptr)((x)*4))
+    #define scm_char(x) ((ptr)(uptr)((x)<<8|0x16))
+    #define scm_nil ((ptr)0x26)
+    #define scm_true ((ptr)0xE)
+    #define scm_false ((ptr)0x6)
+    #define scm_boolean(x) ((x)?Strue:Sfalse)
+    #define scm_bwp_object ((ptr)0x4E)
+    #define scm_eof_object ((ptr)0x36)
+    #define scm_void ((ptr)0x3E)
+
     #endif
     
 #endif
 
-#define scm_fixnum(x) ((ptr)(uptr)((x)*8))
-#define scm_char(x) ((ptr)(uptr)((x)<<8|0x16))
-#define scm_nil ((ptr)0x26)
-#define scm_true ((ptr)0xE)
-#define scm_false ((ptr)0x6)
-#define scm_boolean(x) ((x)?scm_true:scm_false)
-#define scm_bwp_object ((ptr)0x4E)
-#define scm_void ((ptr)0x3E)
-#define scm_eof_object ((ptr)0x36)
 
-#define scm_fixnump(x) (((uptr)(x)&0x7)==0x0)
-#define scm_charp(x) (((uptr)(x)&0xFF)==0x16)
-#define scm_nullp(x) ((uptr)(x)==0x26)
-#define scm_eof_objectp(x) ((uptr)(x)==0x36)
-#define scm_bwp_objectp(x) ((uptr)(x)==0x4E)
-#define scm_booleanp(x) (((uptr)(x)&0xF7)==0x6)
-#define scm_pairp(x) (((uptr)(x)&0x7)==0x1)
-#define scm_symbolp(x) (((uptr)(x)&0x7)==0x3)
-#define scm_procedurep(x) (((uptr)(x)&0x7)==0x5)
-#define scm_flonump(x) (((uptr)(x)&0x7)==0x2)
+
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
+    
 
     int scm_init();
 
@@ -75,6 +89,19 @@ extern "C" {
     iptr scm_fixnum_value(ptr p);
 
     iptr scm_objectp(ptr p);
+
+    iptr scm_fixnump(ptr x);
+    iptr scm_charp(ptr x);
+
+    iptr scm_nullp(ptr x);
+    iptr scm_eof_objectp(ptr x);
+    iptr scm_bwp_objectp(ptr x);
+    iptr scm_booleanp(ptr x);
+    iptr scm_pairp(ptr x);
+    iptr scm_symbolp(ptr x) ;
+    iptr scm_procedurep(ptr x);
+    iptr scm_flonump(ptr x) ;
+
 
 #ifdef __cplusplus
 }

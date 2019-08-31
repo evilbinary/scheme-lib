4209ea14fd0f58a38da765ae9efb2f75a4f011b2
evilbinary <rootntsd@gmail.com>
2019-08-31 09:10:55 +0800

    update function

--- lib/platform/darwin/libscm/scm.h
+++ lib/platform/darwin/libscm/scm.h
@@ -1,109 +1,143 @@
 /**
  * 作者:evilbinary on 10/30/16.
- * 邮箱:rootdebug@163.com 
+ * 邮箱:rootdebug@163.com
  */
 
 #ifndef SCM_H_H
 #define SCM_H_H
 
-
 #ifdef _WIN32
-#  if __cplusplus
-#    ifdef SCM_IMPORT
-#      define SCM_API extern "C" __declspec (dllimport)
-#    elif SCM_STATIC
-#      define SCM_API extern "C"
-#    else
-#      define SCM_API extern "C" __declspec (dllexport)
-#    endif
-#  else
-#    ifdef SCM_IMPORT
-#      define SCM_API extern __declspec (dllimport)
-#    elif SCM_STATIC
-#      define SCM_API extern
-#    else
-#      define SCM_API extern __declspec (dllexport)
-#    endif
-#  endif
+#if __cplusplus
+#ifdef SCM_IMPORT
+#define SCM_API extern "C" __declspec(dllimport)
+#elif SCM_STATIC
+#define SCM_API extern "C"
 #else
-#  if __cplusplus
-#    define SCM_API extern "C"
-#  else
-#    define SCM_API extern
-#  endif
+#define SCM_API extern "C" __declspec(dllexport)
 #endif
+#else
+#ifdef SCM_IMPORT
+#define SCM_API extern __declspec(dllimport)
+#elif SCM_STATIC
+#define SCM_API extern
+#else
+#define SCM_API extern __declspec(dllexport)
+#endif
+#endif
+#else
+#if __cplusplus
+#define SCM_API extern "C"
+#else
+#define SCM_API extern
+#endif
+#endif
+
 
 #if (defined ANDROID) || (defined ARMV6)
-    typedef void *ptr;
-    typedef int iptr;
-    typedef unsigned int uptr;
+typedef void *ptr;
+typedef int iptr;
+typedef unsigned int uptr;
+typedef unsigned int string_char;
 
-    #define scm_fixnum(x) ((ptr)(uptr)((x)*4))
-    #define scm_char(x) ((ptr)(uptr)((x)<<8|0x16))
-    #define scm_nil ((ptr)0x26)
-    #define scm_true ((ptr)0xE)
-    #define scm_false ((ptr)0x6)
-    #define scm_boolean(x) ((x)?scm_true:scm_false)
-    #define scm_bwp_object ((ptr)0x4E)
-    #define scm_eof_object ((ptr)0x36)
-    #define scm_void ((ptr)0x3E)
 #else
-    #ifdef __x86_64__
-    
-    typedef void * ptr;
-    typedef long int iptr;
-    typedef unsigned long int uptr;
-    
-    #define scm_fixnum(x) ((ptr)(uptr)((x)*8))
-    #define scm_char(x)   ((ptr)(uptr)((x)<<8|0x16))
-    #define scm_nil ((ptr)0x26)
-    #define scm_true ((ptr)0xE)
-    #define scm_false ((ptr)0x6)
-    #define scm_boolean(x) ((x)?scm_true:scm_false)
-    #define scm_bwp_object ((ptr)0x4E)
-    #define scm_void ((ptr)0x3E)
-    #define scm_eof_object ((ptr)0x36)
+#ifdef __x86_64__
 
-    #else
-    typedef void * ptr;
-    typedef int iptr;
-    typedef unsigned int uptr;
+typedef void *ptr;
+typedef long int iptr;
+typedef unsigned long int uptr;
+typedef unsigned int string_char;
 
-    #define scm_fixnum(x) ((ptr)(uptr)((x)*4))
-    #define scm_char(x) ((ptr)(uptr)((x)<<8|0x16))
-    #define scm_nil ((ptr)0x26)
-    #define scm_true ((ptr)0xE)
-    #define scm_false ((ptr)0x6)
-    #define scm_boolean(x) ((x)?scm_true:scm_false)
-    #define scm_bwp_object ((ptr)0x4E)
-    #define scm_eof_object ((ptr)0x36)
-    #define scm_void ((ptr)0x3E)
-
-    #endif
-    
+#else
+typedef void *ptr;
+typedef int iptr;
+typedef unsigned int uptr;
+typedef unsigned int string_char;
+#endif
 #endif
 
-    
+SCM_API string_char scm_char_value(ptr x);
+
+#if (defined ANDROID) || (defined ARMV6)
+#define scm_fixnum(x) ((ptr)(uptr)((x)*4))
+#define scm_char(x) ((ptr)(uptr)((x) << 8 | 0x16))
+#define scm_nil ((ptr)0x26)
+#define scm_true ((ptr)0xE)
+#define scm_false ((ptr)0x6)
+#define scm_boolean(x) ((x) ? scm_true : scm_false)
+#define scm_bwp_object ((ptr)0x4E)
+#define scm_eof_object ((ptr)0x36)
+#define scm_void ((ptr)0x3E)
+#define scm_vector_length(x) ((iptr)((uptr)(*((iptr *)((uptr)(x) + 1))) >> 3))
+#define scm_vector_ref(x, i) (((ptr *)((uptr)(x) + 5))[i])
+#define scm_fxvector_length(x) ((iptr)((uptr)(*((iptr *)((uptr)(x) + 1))) >> 4))
+#define scm_fxvector_ref(x, i) (((ptr *)((uptr)(x) + 5))[i])
+
+#define scm_string_length(x) ((iptr)((uptr)(*((iptr *)((uptr)(x) + 1))) >> 4))
+#define scm_string_ref(x, i) scm_char_value(((string_char *)((uptr)(x) + 5))[i])
+
+#else
+#ifdef __x86_64__
+
+#define scm_fixnum(x) ((ptr)(uptr)((x)*8))
+#define scm_char(x) ((ptr)(uptr)((x) << 8 | 0x16))
+#define scm_nil ((ptr)0x26)
+#define scm_true ((ptr)0xE)
+#define scm_false ((ptr)0x6)
+#define scm_boolean(x) ((x) ? scm_true : scm_false)
+#define scm_bwp_object ((ptr)0x4E)
+#define scm_void ((ptr)0x3E)
+#define scm_eof_object ((ptr)0x36)
+#define scm_vector_length(x) ((iptr)((uptr)(*((iptr *)((uptr)(x) + 1))) >> 4))
+#define scm_vector_ref(x, i) (((ptr *)((uptr)(x) + 9))[i])
+
+#define scm_fxvector_length(x) ((iptr)((uptr)(*((iptr *)((uptr)(x) + 1))) >> 4))
+#define scm_fxvector_ref(x, i) (((ptr *)((uptr)(x) + 9))[i])
+#define scm_string_length(x) ((iptr)((uptr)(*((iptr *)((uptr)(x) + 1))) >> 4))
+#define scm_string_ref(x, i) scm_char_value(((string_char *)((uptr)(x) + 9))[i])
+
+#else
+
+#define scm_fixnum(x) ((ptr)(uptr)((x)*4))
+#define scm_char(x) ((ptr)(uptr)((x) << 8 | 0x16))
+#define scm_nil ((ptr)0x26)
+#define scm_true ((ptr)0xE)
+#define scm_false ((ptr)0x6)
+#define scm_boolean(x) ((x) ? scm_true : scm_false)
+#define scm_bwp_object ((ptr)0x4E)
+#define scm_eof_object ((ptr)0x36)
+#define scm_void ((ptr)0x3E)
+
+#define scm_vector_length(x) ((iptr)((uptr)(*((iptr *)((uptr)(x) + 1))) >> 3))
+#define scm_vector_ref(x, i) (((ptr *)((uptr)(x) + 5))[i])
+#define scm_fxvector_length(x) ((iptr)((uptr)(*((iptr *)((uptr)(x) + 1))) >> 4))
+#define scm_fxvector_ref(x, i) (((ptr *)((uptr)(x) + 5))[i])
+
+#define scm_string_length(x) ((iptr)((uptr)(*((iptr *)((uptr)(x) + 1))) >> 4))
+#define scm_string_ref(x, i) scm_char_value(((string_char *)((uptr)(x) + 5))[i])
+
+#endif
+
+#endif
 
 SCM_API int scm_init();
 SCM_API ptr scm_read_string(char *string);
 SCM_API ptr scm_eval(char *string);
 SCM_API ptr scm_eval_exp(ptr exp);
 SCM_API char *scm_strings(ptr p);
-SCM_API ptr scm_string(char * p);
+SCM_API ptr scm_string(char *p);
 SCM_API void scm_deinit();
 SCM_API ptr scm_call0(ptr who);
 SCM_API ptr scm_call1(ptr who, ptr arg);
 SCM_API ptr scm_call2(ptr who, ptr arg0, ptr arg1);
 SCM_API ptr scm_call3(ptr who, ptr arg0, ptr arg1, ptr arg2);
-SCM_API ptr scm_call4(ptr who,ptr arg0,ptr arg1,ptr arg2,ptr arg3);
+SCM_API ptr scm_call4(ptr who, ptr arg0, ptr arg1, ptr arg2, ptr arg3);
 SCM_API ptr scm_call0_proc(ptr proc);
-SCM_API ptr scm_call1_proc(ptr proc,ptr arg);
-SCM_API ptr scm_call2_proc(ptr proc,ptr arg0,ptr arg1);
-SCM_API ptr scm_call3_proc(ptr proc,ptr arg0,ptr arg1,ptr arg2);
-SCM_API ptr scm_call4_proc(ptr proc,ptr arg0,ptr arg1,ptr arg2,ptr arg3);
-SCM_API ptr scm_call5_proc(ptr proc,ptr arg0,ptr arg1,ptr arg2,ptr arg3,ptr arg4);
-
+SCM_API ptr scm_call1_proc(ptr proc, ptr arg);
+SCM_API ptr scm_call2_proc(ptr proc, ptr arg0, ptr arg1);
+SCM_API ptr scm_call3_proc(ptr proc, ptr arg0, ptr arg1, ptr arg2);
+SCM_API ptr scm_call4_proc(ptr proc, ptr arg0, ptr arg1, ptr arg2, ptr arg3);
+SCM_API ptr scm_call5_proc(ptr proc, ptr arg0, ptr arg1, ptr arg2, ptr arg3,
+                           ptr arg4);
 
 SCM_API iptr scm_fixnum_value(ptr p);
 SCM_API iptr scm_objectp(ptr p);
@@ -114,18 +148,17 @@ SCM_API iptr scm_eof_objectp(ptr x);
 SCM_API iptr scm_bwp_objectp(ptr x);
 SCM_API iptr scm_booleanp(ptr x);
 SCM_API iptr scm_pairp(ptr x);
-SCM_API iptr scm_symbolp(ptr x) ;
+SCM_API iptr scm_symbolp(ptr x);
 SCM_API iptr scm_procedurep(ptr x);
-SCM_API iptr scm_flonump(ptr x) ;
+SCM_API iptr scm_flonump(ptr x);
 SCM_API iptr scm_vectorp(ptr x);
+SCM_API iptr scm_stringp(ptr x);
 
 SCM_API ptr scm_flonum(double x);
 SCM_API double scm_flonum_value(ptr x);
 
-SCM_API ptr scm_integer64 (long long);
+SCM_API ptr scm_integer64(long long);
 SCM_API void scm_print(ptr p);
 SCM_API ptr scm_get_thread_context();
 
-
-
-#endif //SCM_H_H
+#endif  // SCM_H_H

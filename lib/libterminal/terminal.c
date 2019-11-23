31b3409fe2e5d80b4c967a6b96408952c037577c
evilbinary <rootntsd@gmail.com>
2019-11-23 16:13:53 +0800

    add terminal resize

--- lib/libterminal/terminal.c
+++ lib/libterminal/terminal.c
@@ -36,7 +36,7 @@ static void term_read_cb(struct shl_pty *pty, char *u8, size_t len,
  * output) */
 static void term_write_cb(struct tsm_vte *vtelocal, const char *u8, size_t len,
                           void *data) {
-  //printf("term_write_cb %s\n", u8);
+  // printf("term_write_cb %s\n", u8);
   terminal *term = (terminal *)data;
   int r;
   r = shl_pty_write(term->pty, u8, len);
@@ -82,21 +82,22 @@ static int draw_cb(struct tsm_screen *screen, uint32_t id, const uint32_t *ch,
   }
   // color = glfonsRGBA(fr,fg,fb,255);
   // fonsSetColor(data, color);
-  
+
   sth_draw_text(term->font->stash, term->font->id, term->font->size, term->x,
-                term->y, tsm_screen_get_width(term->console) , -1, ch, fr, fg, fb, 1.0, &dx, &dy);
+                term->y, tsm_screen_get_width(term->console), -1, ch, fr, fg,
+                fb, 1.0, &dx, &dy);
   // printf("%f %f\n",term->x,term->y);
-  
+
   if (posx >= (screen->size_x - 1)) {
     term->x = term->starx;
-	term->y = dy + term->ascender;
-  }else{
-	term->x = dx;
+    term->y = dy + term->lineh;
+  } else {
+    term->x = dx;
   }
 
   if (posy > (screen->size_y - 1)) {
     term->x = term->starx;
-	term->y = term->stary;
+    term->y = term->stary;
   }
 
   return 0;
@@ -133,69 +134,68 @@ void init_pty(terminal *term) {
   }
 }
 
-void terminal_set_mvp(terminal* term,mvp_t *mvp,float width ,float height){
-  term->mvp=mvp;
+void terminal_resize(terminal *term, float width, float height) {
+  if(width<0||height<0) return;
+  float w = measure_text(term->font, term->font->size, "W", -1);
+  float h = graphic_get_font_height(term->font, term->font->size);
+  tsm_screen_resize(term->console, (width* term->scale / w) - 1, (height* term->scale / (h+term->lineh )+2 ));
+  shl_pty_resize(term->pty, tsm_screen_get_width(term->console),
+                 tsm_screen_get_height(term->console));
+}
+
+void terminal_set_mvp(terminal *term, mvp_t *mvp, float width, float height) {
+  *term->mvp = *mvp;
   mat4_set_identity(&term->mvp->projection);
   mat4_set_identity(&term->mvp->model);
   mat4_set_identity(&term->mvp->view);
   mat4_set_orthographic(&term->mvp->projection, 0, width * term->scale,
                         height * term->scale, 0, 1, -1);
-
 }
 
-void terminal_resize(terminal* term,int width,int height){
-  float w=measure_text(term->font,term->font->size,"W",-1);
-  tsm_screen_resize(term->console, (width / w)-1 , (height / term->lineh)-1);
-  shl_pty_resize(term->pty, tsm_screen_get_width(term->console), tsm_screen_get_height(term->console));
-}
-
-terminal *terminal_create(font_t *font ,float size,float width,float height) {
+terminal *terminal_create(font_t *font, float size, float width, float height) {
   terminal *term = malloc(sizeof(terminal));
-  if(font!=NULL){
-	  term->font=font;
-  }else{
-  	term->font = new_font("./Roboto-Regular.ttf", size);
+  if (font != NULL) {
+    term->font = font;
+  } else {
+    term->font = new_font("./Roboto-Regular.ttf", size);
   }
   term->lineh = 10.0;
   term->linew = 20.0;
   term->ascender = 10.0;
-  term->scale=2;
+  term->scale = 2;
   term->x = 0;
   term->y = 0;
-  term->mvp=NULL;
-
+  term->mvp = malloc(sizeof(mvp_t));
+  term->width=width;
+  term->height=height;
   tsm_screen_new(&term->console, log_tsm, 0);
   tsm_vte_new(&term->vte, term->console, term_write_cb, term, log_tsm, 0);
-  // tsm_screen_resize(console, (width / (bounds[2]-bounds[0]) - 1), (height /
-  // lineh)-1);
+  init_pty(term);
 
+  terminal_resize(term, (int)width , (int)height );
   printf("console width: %d\n", tsm_screen_get_width(term->console));
   printf("console height: %d\n", tsm_screen_get_height(term->console));
   tsm_vte_get_def_attr(term->vte, &term->attr);
 
   // char* test="\033[31m red \033[0m \n\033[32m green \033[0m\n\033[33m yellow
   // \033[0m"; tsm_vte_input(term->vte, test, strlen(test));
-
-  init_pty(term);
-
   return term;
 }
 
-
-void terminal_draw(terminal* term,float x,float y){
+void terminal_draw(terminal *term, float x, float y) {
   term->x = term->starx;
   term->y = term->stary;
-  term->starx=x*term->scale;
-  term->stary=y*term->scale;
-  if(term->mvp==NULL) return;
+  term->starx = x * term->scale;
+  term->stary = y * term->scale;
+  if (term->mvp == NULL) return;
   graphic_set_mvp(term->mvp);
   sth_begin_draw(term->font->stash);
   tsm_screen_draw(term->console, draw_cb, term);
-  graphic_render_end_string();
+  sth_end_draw(term->font->stash);
 }
-void terminal_render(terminal *term,float x ,float y) {
+void terminal_render(terminal *term, float x, float y) {
   shl_pty_dispatch(term->pty);
-  terminal_draw(term,x,y);
+  terminal_draw(term, x, y);
 }
 
 void terminal_key_event(terminal *term, int key, int scancode, int action,
@@ -217,9 +217,9 @@ void terminal_key_event(terminal *term, int key, int scancode, int action,
     if (scancode == XKB_KEY_Up || scancode == XKB_KEY_Down ||
         scancode == XKB_KEY_Left || scancode == XKB_KEY_Right ||
         scancode == XKB_KEY_Return || scancode == XKB_KEY_Delete) {
-      printf("mode %d key=%d scancode=%0x\n", mod,key, scancode);
+      printf("mode %d key=%d scancode=%0x\n", mod, key, scancode);
       tsm_vte_handle_keyboard(term->vte, scancode, key, mod, key);
-	//   terminal_draw(term);
+      //   terminal_draw(term);
     }
   }
   // tsm_vte_get_def_attr(term->vte, &term->attr);
@@ -228,7 +228,7 @@ void terminal_char_event(terminal *term, int ch, int mods) {
   char buf[20] = {0};
   sprintf(buf, "%c", ch);
   // tsm_vte_input(term->vte, buf, strlen(buf));
-  //printf("terminal_char_event %c\n",buf);
+  // printf("terminal_char_event %c\n",buf);
   tsm_vte_handle_keyboard(term->vte, 0, ch, 0, ch);
 }
 
